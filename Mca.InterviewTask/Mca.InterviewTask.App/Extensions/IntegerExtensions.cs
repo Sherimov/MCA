f15df160c4847c6a290872253b51b9104934ad82
Sherimov <60267865+Sherimov@users.noreply.github.com>
2021-06-10 15:28:05 +0200

    Add files via upload

---
+++ Mca.InterviewTask/Mca.InterviewTask.App/Extensions/IntegerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mca.InterviewTask.App.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static string PrintWeight(this int weight)
+        {
+
+            return weight == 0 ? "N/A" : $"{weight}g";
+        }
+
+    }
+}
---
+++ Mca.InterviewTask/Mca.InterviewTask.App/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mca.InterviewTask.App.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value)
+        {
+            return value.Length <= 30 ? value : value.Substring(0, 30) + "...";
+        }
+    }
+}
